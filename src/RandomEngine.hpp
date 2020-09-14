3b4bfab78bfcf3a91311e3f313931b1c5ac9ce0c
Sampuka <matddi@gmail.com>
2020-09-14 14:54:13 +0200

    Tells if about to segfault like a good boy

--- src/RandomEngine.hpp
+++ src/RandomEngine.hpp
@@ -15,6 +15,11 @@ public:
     {
         std::vector<Move> moves = board.getMoves();
 
+        if (moves.size() == 0)
+        {
+            std::cerr << "No legal moves found!" << std::endl;
+        }
+
         std::uniform_int_distribution<std::uint8_t> dist(0, moves.size()-1);
 
         bestmove = moves.at(dist(eng));
