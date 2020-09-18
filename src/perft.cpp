59f7d4999b433b3733c08ec103c066bd70356758
Sampuka <matddi@gmail.com>
2020-09-18 21:17:37 +0200

    Improved perft

--- src/perft.cpp
+++ src/perft.cpp
@@ -7,18 +7,26 @@
 int main(int argc, char** argv)
 {
     int goal = 6;
+    std::string pos = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -";
 
     if (argc > 1)
         goal = std::atoi(argv[1]);
 
-    Board base;
-
     if (argc > 2)
     {
         if (argv[2] == std::string("kiwipete"))
-            base = Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+            pos = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
+        else if (argv[2] == std::string("pos3"))
+            pos = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -";
+        else if (argv[2] == std::string("pos4"))
+            pos = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq -";
+        else if (argv[2] == std::string("pos5"))
+            pos = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ -";
+        else if (argv[2] == std::string("pos6"))
+            pos = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - -";
     }
 
+    Board base(pos);
     base.print();
 
     for (int i = 1; i <= goal; i++)
