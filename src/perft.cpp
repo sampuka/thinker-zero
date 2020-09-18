079eca4619ddc47d64608d9bbf6febe51dcc29e0
Sampuka <matddi@gmail.com>
2020-09-18 17:58:35 +0200

    Changed to Kiwipete position

--- src/perft.cpp
+++ src/perft.cpp
@@ -10,7 +10,11 @@ int main(int argc, char** argv)
     if (argc == 2)
         goal = std::atoi(argv[1]);
 
-    Board base;
+    // Kiwipete
+    Board base("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+
+    // Standard
+    //Board base;
 
     for (int i = 1; i <= goal; i++)
     {
