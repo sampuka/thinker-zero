a25f99ab2dad67ad0f98c9d6e3c2434532c97684
Sampuka <matddi@gmail.com>
2020-09-18 19:24:11 +0200

    Improved perft

--- src/perft.cpp
+++ src/perft.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <string>
 
 #include "Board.hpp"
 
@@ -7,16 +8,19 @@ int main(int argc, char** argv)
 {
     int goal = 6;
 
-    if (argc == 2)
+    if (argc > 1)
         goal = std::atoi(argv[1]);
 
-    // Kiwipete
-    Board base("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+    Board base;
 
-    // Standard
-    //Board base;
+    if (argc > 2)
+    {
+        if (argv[2] == std::string("kiwipete"))
+            base = Board("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+    }
 
     base.print();
+
     for (int i = 1; i <= goal; i++)
     {
         BoardTree tree(base);
