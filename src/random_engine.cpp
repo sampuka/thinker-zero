a125268039bd3c6988646181f7137b4fb0c7ee1b
sampuka <matddi@gmail.com>
2020-09-22 10:52:30 +0200

    cleaned up

--- src/random_engine.cpp
+++ src/random_engine.cpp
@@ -1,4 +1,33 @@
-#include "RandomEngine.hpp"
+#include "UCIEngine.hpp"
+
+class RandomEngine : public UCIEngine
+{
+public:
+    RandomEngine()
+    {
+        start();
+    }
+
+    void think() override
+    {
+        std::vector<Move> moves = board.getMoves();
+
+        if (moves.size() == 0)
+        {
+            std::cerr << "No legal moves found!" << std::endl;
+        }
+        else
+        {
+            for (const Move &move : moves)
+                log << move.longform() << std::endl;
+        }
+
+        std::uniform_int_distribution<std::uint8_t> dist(0, moves.size()-1);
+
+        bestmove = moves.at(dist(eng));
+        thinking = false;
+    }
+};
 
 int main()
 {
