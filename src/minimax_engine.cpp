ab523be6faa8b3e2bf4d33cd7dc7fc382e8b840b
Muligheder <anders.pedersen7@gmail.com>
2020-09-29 12:44:53 +0200

    added minimaxinit

---
+++ src/minimax_engine.cpp
@@ -0,0 +1,37 @@
+#include "UCIEngine.hpp"
+
+class RandomEngine : public UCIEngine
+{
+public:
+    RandomEngine()
+    {
+        start();
+    }
+
+    void think() override
+    {
+        std::vector<Move> moves = board.getMoves();
+
+        if (moves.size() == 0)
+        {
+            std::cerr << "No legal moves found!" << std::endl;
+        }
+        else
+        {
+            for (const Move &move : moves)
+                log << move.longform() << std::endl;
+        }
+
+        std::uniform_int_distribution<std::uint8_t> dist(0, moves.size()-1);
+
+        bestmove = moves.at(dist(eng));
+        thinking = false;
+    }
+};
+
+int main()
+{
+    RandomEngine engine;
+
+    return 0;
+}
