2b9ae5bb9de9d53de3365f751498ea34fc1bd372
mathiaslyngbye <malyn16@student.sdu.dk>
2020-10-01 11:00:12 +0200

    Simple update

--- src/abp_engine.cpp
+++ src/abp_engine.cpp
@@ -10,6 +10,11 @@ public:
 
     void think() override
     {
+        // Default white 
+        int turn = 1;
+        if(board.getTurn() == Color::Black)
+            turn = -1;
+
         // Get possible moves
         std::vector<Move> moves = board.getMoves();
         if (moves.size() == 0)
@@ -23,12 +28,23 @@ public:
         }
    
         // Find random move among moves
-        std::uniform_int_distribution<std::uint8_t> dist(0, moves.size()-1);
-        bestmove = moves.at(dist(eng));
-        
-        // Create test board
         Board test_board(board);
         test_board.performMove(moves.at(0));
+        double bestvalue = test_board.basic_eval() * turn;
+        bestmove = moves.at(0);     // Default first move in case rest fails
+        for (const Move &move : moves)
+        {
+            test_board = board;
+            test_board.performMove(move);
+            
+            double eval = test_board.basic_eval() * turn;
+            if(eval >= bestvalue)
+            {
+                bestvalue = eval;
+                bestmove = move;
+            }
+        }
+        std::cout << "Best value: " << bestvalue << std::endl;
         
         // End of function
         thinking = false;
