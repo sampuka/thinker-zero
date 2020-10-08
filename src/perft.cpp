f791372a2c8bb9d5c029e19107b7239026f0cb0b
Sampuka <matddi@gmail.com>
2020-10-08 23:56:02 +0200

    Movegen testing

--- src/perft.cpp
+++ src/perft.cpp
@@ -14,11 +14,13 @@ int main(int argc, char** argv)
         //movegen_rays[static_cast<std::uint8_t>(Ray::NE)][25].print(); std::cout << '\n';
         //movegen_rays[static_cast<std::uint8_t>(Ray::N)][10].print(); std::cout << '\n';
         //movegen_rays[static_cast<std::uint8_t>(Ray::E)][4].print(); std::cout << '\n';
-        movegen_rays[static_cast<std::uint8_t>(Ray::BlackPawnAttacks)][32].print(); std::cout << '\n';
+        //movegen_rays[static_cast<std::uint8_t>(Ray::BlackPawnAttacks)][32].print(); std::cout << '\n';
         //movegen_rays[static_cast<std::uint8_t>(Ray::King)][10].print(); std::cout << '\n';
 
         //Board test_board("8/1p6/1Q3b2/6P1/3B1N2/8/P4R2/7K w - - 1 0");
-        Board test_board;
+        //Board test_board("8/8/8/8/8/8/6k1/4K2R w K - 0 1");
+        Board test_board("rnbqkbnr/ppp1pppp/3p4/1B6/8/4P3/PPPP1PPP/RNBQK1NR b KQkq - 1 2 ");
+        //Board test_board;
         test_board.print();
 
         auto movelist = test_board.ray_movegen();
