aebe089c21a70464a339159393c46e717531098c
mathiaslyngbye <malyn16@student.sdu.dk>
2020-10-27 13:58:19 +0100

    Add find_best_child function, and test time

--- src/mcts_engine.cpp
+++ src/mcts_engine.cpp
@@ -22,43 +22,36 @@ public:
 
         MoveList startmoves;
         board.get_moves(startmoves);
+        bestmove = startmoves.at(1); // Initialize with something (test)
 
-        bestmove = startmoves.at(1);
-
-        /*
         // Create root
         BoardTree root(board);
 
-        // Selection
-        BoardTree node = root;
-        while(true)
+        while(time_spent < 5000)
         {
-            // Break if leaf node
-            if(node.nodes.size() == 0)
-                break;
-
-            // Find best child of node
-            double best_child_evaluation = -999999;
-            BoardTree best_child;
-            for (const BoardTree& child : node.nodes)
-            {
-                if((child.evaluation*turn_bias) > best_child_evaluation)
-                {
-                    best_child_evalutaion = child.evaluation;
-                    best_child = child;
-                }
-            }
-
-            // Select best child
+            ;//std::cout << time_spent << std::endl;
         }
-        */
 
         // End of function
         thinking = false;
     }
 
-    double uct(int parent_visit, int node_visit, double node_score, double c)
+    const BoardTree *find_best_child(const BoardTree &node)
     {
+        int parent_visitcount = node.visitcount;
+        const auto comparator = [&](const BoardTree &a, const BoardTree &b)
+        {
+            double node_a_uct = uct(parent_visitcount, a.winscore, a.visitcount);
+            double node_b_uct = uct(parent_visitcount, b.winscore, b.visitcount);
+            return (node_a_uct < node_b_uct);
+        };
+
+        return &(*std::max_element(node.nodes.begin(), node.nodes.end(), comparator));
+    }
+
+    double uct(int parent_visit, int node_visit, double node_score)
+    {
+        double c = std::sqrt(2.0); // Theoretical value
         return ((node_score / node_visit) + c * std::sqrt(std::log(parent_visit) / node_visit));
     }
 };
