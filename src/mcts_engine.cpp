64e8afd9eb346abb6ae4f116b706c8e8225743f7
mathiaslyngbye <malyn16@student.sdu.dk>
2020-10-27 16:02:52 +0100

    Finish selection and expansion - start playout

--- src/mcts_engine.cpp
+++ src/mcts_engine.cpp
@@ -3,11 +3,13 @@
 #include <functional>
 #include <cmath>
 
-class MinimaxEngine : public UCIEngine
+class MctsEngine : public UCIEngine
 {
 public:
-    MinimaxEngine()
+    MctsEngine()
     {
+        engine_author = "Mathias Lyngbye";
+        engine_name = "MCTS Engine";
         start();
     }
 
@@ -16,29 +18,70 @@ public:
     void think() override
     {
         // Default white
+        /*
         int turn_bias = 1;
         if(board.get_turn() == Color::Black)
             turn_bias = -1;
+        */
 
+        // Initialization
         MoveList startmoves;
         board.get_moves(startmoves);
-        bestmove = startmoves.at(1); // Initialize with something (test)
+        bestmove = startmoves.at(1);
 
-        // Create root
-        BoardTree root(board);
+        // Root node
+        BoardTree root_node(board);
 
-        while(time_spent < 5000)
+        // MCTS
+        uint64_t time_max = 2000;
+        while(time_spent < time_max)
         {
-            ;//std::cout << time_spent << std::endl;
+            // Selection
+            BoardTree *promising_node = select_node(&root_node);
+
+            // Expansion
+            promising_node->expand(movelist);
         }
 
         // End of function
         thinking = false;
     }
 
-    const BoardTree *find_best_child(const BoardTree &node)
+    int simulate_playout_random(BoardTree *root_node)
     {
-        int parent_visitcount = node.visitcount;
+        MoveList tmp_moves;
+        
+        Board tmp_board = root_node->board;
+        tmp_board.get_moves(tmp_moves);
+
+        while(!tmp_board.is_checkmate(tmp_moves) && !tmp_board.is_stalemate(tmp_moves))
+        {
+            // Generate random move
+            std::uniform_int_distribution<std::uint8_t> dist(0, tmp_moves.size()-1);
+            Move random_move = tmp_moves.at(dist(eng));
+            
+            tmp_board.perform_move(random_move);
+            tmp_board.get_moves(tmp_moves);
+        }
+
+        // NOTE: This function is not finished.
+        return 0;
+    }
+
+    BoardTree *select_node(BoardTree *root_node)
+    {
+        BoardTree *node = root_node;
+        while(node->nodes.size() != 0)  // While not a leaf
+        {
+            node = find_best_child(node);
+        }
+        
+        return node;
+    }
+
+    BoardTree *find_best_child(BoardTree *node)
+    {
+        int parent_visitcount = node->visitcount;
         const auto comparator = [&](const BoardTree &a, const BoardTree &b)
         {
             double node_a_uct = uct(parent_visitcount, a.winscore, a.visitcount);
@@ -46,7 +89,7 @@ public:
             return (node_a_uct < node_b_uct);
         };
 
-        return &(*std::max_element(node.nodes.begin(), node.nodes.end(), comparator));
+        return &(*std::max_element(node->nodes.begin(), node->nodes.end(), comparator));
     }
 
     double uct(int parent_visit, int node_visit, double node_score)
@@ -58,7 +101,7 @@ public:
 
 int main()
 {
-    MinimaxEngine engine;
+    MctsEngine engine;
 
     return 0;
 }
