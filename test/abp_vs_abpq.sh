56c056c85c207ba25f4fde18fcae916b5efbb402
sampuka <matddi@gmail.com>
2021-01-21 16:39:44 +0100

    Tests

--- test/abp_vs_abpq.sh
+++ test/abp_vs_abpq.sh
@@ -3,4 +3,14 @@
 cutechess-cli \
     -engine cmd=../build/abp_engine \
     -engine cmd=../build/abpq_engine \
-    -each proto=uci tc=40/60 -rounds 30
+    -each proto=uci tc=0/600+5 -rounds 10
+
+cutechess-cli \
+    -engine cmd=../build/abp_engine \
+    -engine cmd=../build/abpq_engine \
+    -each proto=uci tc=40/120+1 -rounds 10
+
+cutechess-cli \
+    -engine cmd=../build/abp_engine \
+    -engine cmd=../build/abpq_engine \
+    -each proto=uci tc=40/30+1 -rounds 10
