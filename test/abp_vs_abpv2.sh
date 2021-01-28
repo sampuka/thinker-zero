56c9da26cf6bbf5d21ec01e67bf9f0408decad69
sampuka <matddi@gmail.com>
2021-01-28 13:07:09 +0100

    Testing

---
+++ test/abp_vs_abpv2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cutechess-cli \
+    -engine cmd=../build/abp_engine \
+    -engine cmd=../build/abp_v2_engine \
+    -each proto=uci tc=40/30+1 -rounds 10
+
+cutechess-cli \
+    -engine cmd=../build/abp_engine \
+    -engine cmd=../build/abp_v2_engine \
+    -each proto=uci tc=40/120+1 -rounds 10
+
+cutechess-cli \
+    -engine cmd=../build/abp_engine \
+    -engine cmd=../build/abp_v2_engine \
+    -each proto=uci tc=0/600+5 -rounds 10
