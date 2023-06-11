b552e9ebefb2198cc3077d1a4d97f06c64062eec
Mathias Ebbensgaard Jensen <matddi@gmail.com>
2023-06-11 14:39:22 +0200

    idk2

--- src/console/console_parsing.cpp
+++ src/console/console_parsing.cpp
@@ -1,10 +1,5 @@
 #include "console_parsing.hpp"
 
-#include "console/position_printer.hpp"
-#include "console/uci_input.hpp"
-#include "engine/Engine.hpp"
-#include "logging/logging.hpp"
-
 #include <iostream>
 #include <memory>
 #include <sstream>
@@ -13,142 +8,146 @@
 #include <tuple>
 #include <vector>
 
+#include "console/position_printer.hpp"
+#include "console/uci_input.hpp"
+#include "engine/Engine.hpp"
+#include "logging/logging.hpp"
+
+
 enum class EngineInterface
 {
-    NONE,
-    UCI
+	NONE,
+	UCI
 };
 
-std::unique_ptr<std::jthread> console_interface_thread = nullptr;
+// std::unique_ptr<std::jthread> console_interface_thread = nullptr;
 bool console_interface_running = true;
 
-EngineInterface selected_interface = EngineInterface::UCI; // Assume UCI for now
+EngineInterface selected_interface = EngineInterface::UCI;  // Assume UCI for now
 
 void parse_uci_command(const std::string& command, const std::vector<std::string>& args)
 {
-    if (command == "isready")
-    {
-        uci_isready();
-    }
-    else if (command == "ucinewgame")
-    {
-        uci_ucinewgame();
-    }
-    else if (command == "position")
-    {
-        if (args.size() >= 1)
-        {
-            uci_position(args);
-        }
-        else
-        {
-            log_error("Position command without arguments");
-            std::printf("Command 'position' requires FEN string\n");
-        }
-    }
-    else if (command == "go")
-    {
-        uci_go(args);
-    }
-    else
-    {
-        std::printf("Unknown command: '%s'\n", command.c_str());
-        log_error("Unknown command: '%s'", command.c_str());
-    }
-
+	if (command == "isready")
+	{
+		uci_isready();
+	}
+	else if (command == "ucinewgame")
+	{
+		uci_ucinewgame();
+	}
+	else if (command == "position")
+	{
+		if (args.size() >= 1)
+		{
+			uci_position(args);
+		}
+		else
+		{
+			log_error("Position command without arguments");
+			std::printf("Command 'position' requires FEN string\n");
+		}
+	}
+	else if (command == "go")
+	{
+		uci_go(args);
+	}
+	else
+	{
+		std::printf("Unknown command: '%s'\n", command.c_str());
+		log_error("Unknown command: '%s'", command.c_str());
+	}
 }
 
 void parse_command(const std::string& command, const std::vector<std::string>& args)
 {
-    if (command == "")
-    {
-        // Just do nothing for empty input
-    }
-    else if (command == "help" || command == "?")
-    {
-        std::printf(
-            "The program intends to support the UCI standard\n"
-            "Available commands (UCI omitted):\n\n"
-            "quit\n"
-            "  Quits application\n\n"
-            );
-    }
-    else if (command == "quit")
-    {
-        console_interface_running = false;
-        engine.shutdown();
-    }
-    else if (command == "print")
-    {
-        std::string position_string = format_position_to_string(engine.get_position());
-        
-        std::printf("Current position:\n%s", position_string.c_str());
-    }
-    else if (command == "uci")
-    {
-        selected_interface = EngineInterface::UCI;
-        uci_start();
-    }
-    else
-    {
-        switch (selected_interface)
-        {
-            case EngineInterface::UCI:
-            {
-                parse_uci_command(command, args);
-                break;
-            }
+	if (command == "")
+	{
+		// Just do nothing for empty input
+	}
+	else if (command == "help" || command == "?")
+	{
+		std::printf(
+			"The program intends to support the UCI standard\n"
+			"Available commands (UCI omitted):\n\n"
+			"quit\n"
+			"  Quits application\n\n");
+	}
+	else if (command == "quit")
+	{
+		console_interface_running = false;
+		engine.shutdown();
+	}
+	else if (command == "print")
+	{
+		std::string position_string = format_position_to_string(engine.get_position());
 
-            default:
-            {
-                log_error("Unknown command for selected interface");
-                std::printf("Unknown command for selected interface\n");
-            }
-        }
-    }
+		std::printf("Current position:\n%s", position_string.c_str());
+	}
+	else if (command == "uci")
+	{
+		selected_interface = EngineInterface::UCI;
+		uci_start();
+	}
+	else
+	{
+		switch (selected_interface)
+		{
+			case EngineInterface::UCI:
+			{
+				parse_uci_command(command, args);
+				break;
+			}
+
+			default:
+			{
+				log_error("Unknown command for selected interface");
+				std::printf("Unknown command for selected interface\n");
+			}
+		}
+	}
 }
 
 std::tuple<std::string, std::vector<std::string>> parse_input(const std::string& input_line)
 {
-    std::string command;
-    std::vector<std::string> args;
+	std::string command;
+	std::vector<std::string> args;
 
-    std::stringstream ss(input_line);
-    std::string token;
+	std::stringstream ss(input_line);
+	std::string token;
 
-    std::getline(ss, command, ' ');
+	std::getline(ss, command, ' ');
 
-    while (std::getline(ss, token, ' '))
-    {
-        args.push_back(token);
-    }
+	while (std::getline(ss, token, ' '))
+	{
+		args.push_back(token);
+	}
 
-    return {command, args};
+	return {command, args};
 }
 
 void console_interface_thread_loop()
 {
-    std::printf("Thinker-zero Chess Engine by Mathias Ebbensgaard Jensen\n");
+	std::printf("Thinker-zero Chess Engine by Mathias Ebbensgaard Jensen\n");
 
-    while (console_interface_running)
-    {        
-        std::string input_line;
-        std::getline(std::cin, input_line);
+	while (console_interface_running)
+	{
+		std::string input_line;
+		std::getline(std::cin, input_line);
 
-        log_msg("> %s", input_line.c_str());
+		log_msg("> %s", input_line.c_str());
 
-        auto [command, args] = parse_input(input_line);
+		auto [command, args] = parse_input(input_line);
 
-        parse_command(command, args);
-    }
+		parse_command(command, args);
+	}
 }
 
 void start_console_interface_thread()
 {
-    console_interface_thread = std::make_unique<std::jthread>(console_interface_thread_loop);
+	// console_interface_thread = std::make_unique<std::jthread>(console_interface_thread_loop);
 }
 
 void end_console_interface_thread()
 {
-    console_interface_thread = nullptr;
+	// console_interface_thread = nullptr;
 }
--- src/movegen/movegen_rays.hpp
+++ src/movegen/movegen_rays.hpp
@@ -1,202 +1,200 @@
 #ifndef MOVEGEN_MOVEGEN_RAYS_HPP
 #define MOVEGEN_MOVEGEN_RAYS_HPP
 
-#include "types/Bitboard.hpp"
-#include "types/Square.hpp"
-
 #include <array>
 #include <functional>
 
+#include "types/Bitboard.hpp"
+#include "types/Square.hpp"
+
 enum class Ray
 {
-    NE = 0,
-    E,
-    SE,
-    S,
-    SW,
-    W,
-    NW,
-    N,
-    WhitePawnAttacks,
-    BlackPawnAttacks,
-    Knight,
-    King
+	NE = 0,
+	E,
+	SE,
+	S,
+	SW,
+	W,
+	NW,
+	N,
+	WhitePawnAttacks,
+	BlackPawnAttacks,
+	Knight,
+	King
 };
 
 constexpr uint8_t abs_distance(uint8_t a, uint8_t b)
 {
-    return (a > b) ? (a - b) : (b - a);
+	return (a > b) ? (a - b) : (b - a);
 }
 
-constexpr std::array<std::array<Bitboard, 64>, 12> movegen_rays = []()
-{
-    std::array<std::array<Bitboard, 64>, 12> rays;
+constexpr std::array<std::array<Bitboard, 64>, 12> movegen_rays = []() {
+	std::array<std::array<Bitboard, 64>, 12> rays;
 
-    for (uint8_t _r = 0; _r < 12; _r++)
-    {
-        const Ray ray = static_cast<Ray>(_r);
+	for (uint8_t _r = 0; _r < 12; _r++)
+	{
+		const Ray ray = static_cast<Ray>(_r);
 
-        for (uint8_t index = 0; index < 64; index++)
-        {
-            const Square from_square(index);
+		for (uint8_t index = 0; index < 64; index++)
+		{
+			const Square from_square(index);
 
-            const uint8_t file = from_square.get_file();
-            const uint8_t rank = from_square.get_rank();
+			const uint8_t file = from_square.get_file();
+			const uint8_t rank = from_square.get_rank();
 
-            auto generate_by_ray = [&] (uint8_t file_dir, uint8_t rank_dir)
-            {
-                uint8_t new_file = file + file_dir;
-                uint8_t new_rank = rank + rank_dir;
+			auto generate_by_ray = [&](uint8_t file_dir, uint8_t rank_dir) {
+				uint8_t new_file = file + file_dir;
+				uint8_t new_rank = rank + rank_dir;
 
-                while (new_file >= FILE_A && new_file <= FILE_H && new_rank >= RANK_1 && new_rank <= RANK_8)
-                {
-                    Square to_square(new_file, new_rank);
+				while (new_file >= FILE_A && new_file <= FILE_H && new_rank >= RANK_1 && new_rank <= RANK_8)
+				{
+					Square to_square(new_file, new_rank);
 
-                    rays[_r][index].set_by_square(to_square);
+					rays[_r][index].set_by_square(to_square);
 
-                    new_file += file_dir;
-                    new_rank += rank_dir;
-                }
-            };
+					new_file += file_dir;
+					new_rank += rank_dir;
+				}
+			};
 
-            switch (ray)
-            {
-                case Ray::NE:
-                {
-                    generate_by_ray(1, 1);
-                    break;
-                }
+			switch (ray)
+			{
+				case Ray::NE:
+				{
+					generate_by_ray(1, 1);
+					break;
+				}
 
-                case Ray::E:
-                {
-                    generate_by_ray(1, 0);
-                    break;
-                }
+				case Ray::E:
+				{
+					generate_by_ray(1, 0);
+					break;
+				}
 
-                case Ray::SE:
-                {
-                    generate_by_ray(1, -1);
-                    break;
-                }
+				case Ray::SE:
+				{
+					generate_by_ray(1, -1);
+					break;
+				}
 
-                case Ray::S:
-                {
-                    generate_by_ray(0, -1);
-                    break;
-                }
+				case Ray::S:
+				{
+					generate_by_ray(0, -1);
+					break;
+				}
 
-                case Ray::SW:
-                {
-                    generate_by_ray(-1, -1);
-                    break;
-                }
+				case Ray::SW:
+				{
+					generate_by_ray(-1, -1);
+					break;
+				}
 
-                case Ray::W:
-                {
-                    generate_by_ray(-1, 0);
-                    break;
-                }
+				case Ray::W:
+				{
+					generate_by_ray(-1, 0);
+					break;
+				}
 
-                case Ray::NW:
-                {
-                    generate_by_ray(-1, 1);
-                    break;
-                }
+				case Ray::NW:
+				{
+					generate_by_ray(-1, 1);
+					break;
+				}
 
-                case Ray::N:
-                {
-                    generate_by_ray(0, 1);
-                    break;
-                }
+				case Ray::N:
+				{
+					generate_by_ray(0, 1);
+					break;
+				}
 
-                case Ray::WhitePawnAttacks:
-                {
-                    if (rank != RANK_1 && rank != RANK_8)
-                    {
-                        if (file != FILE_A)
-                        {
-                            Square to_square(file - 1, rank + 1);
-                            rays[_r][index].set_by_square(to_square);
-                        }
-                        if (file != FILE_H)
-                        {
-                            Square to_square(file + 1, rank + 1);
-                            rays[_r][index].set_by_square(to_square);
-                        }
-                    }
-                    break;
-                }
+				case Ray::WhitePawnAttacks:
+				{
+					if (rank != RANK_1 && rank != RANK_8)
+					{
+						if (file != FILE_A)
+						{
+							Square to_square(file - 1, rank + 1);
+							rays[_r][index].set_by_square(to_square);
+						}
+						if (file != FILE_H)
+						{
+							Square to_square(file + 1, rank + 1);
+							rays[_r][index].set_by_square(to_square);
+						}
+					}
+					break;
+				}
 
-                case Ray::BlackPawnAttacks:
-                {
-                    if (rank != RANK_1 && rank != RANK_8)
-                    {
-                        if (file != FILE_A)
-                        {
-                            Square to_square(file - 1, rank - 1);
-                            rays[_r][index].set_by_square(to_square);
-                        }
-                        if (file != FILE_H)
-                        {
-                            Square to_square(file + 1, rank - 1);
-                            rays[_r][index].set_by_square(to_square);
-                        }
-                    }
-                    break;
-                }
+				case Ray::BlackPawnAttacks:
+				{
+					if (rank != RANK_1 && rank != RANK_8)
+					{
+						if (file != FILE_A)
+						{
+							Square to_square(file - 1, rank - 1);
+							rays[_r][index].set_by_square(to_square);
+						}
+						if (file != FILE_H)
+						{
+							Square to_square(file + 1, rank - 1);
+							rays[_r][index].set_by_square(to_square);
+						}
+					}
+					break;
+				}
 
-                case Ray::Knight:
-                {
-                    uint8_t min_file = (file <= FILE_B) ? FILE_A : (file - 2);
-                    uint8_t max_file = (file >= FILE_G) ? FILE_H : (file + 2);
+				case Ray::Knight:
+				{
+					uint8_t min_file = (file <= FILE_B) ? FILE_A : (file - 2);
+					uint8_t max_file = (file >= FILE_G) ? FILE_H : (file + 2);
 
-                    uint8_t min_rank = (rank <= RANK_2) ? RANK_1 : (rank - 2);
-                    uint8_t max_rank = (rank >= RANK_7) ? RANK_8 : (rank + 2);
-                    
-                    for (uint8_t to_file = min_file; to_file <= max_file; to_file++)
-                    {
-                        for (uint8_t to_rank = min_rank; to_rank <= max_rank; to_rank++)
-                        {
-                            Square to_square(to_file, to_rank);
+					uint8_t min_rank = (rank <= RANK_2) ? RANK_1 : (rank - 2);
+					uint8_t max_rank = (rank >= RANK_7) ? RANK_8 : (rank + 2);
 
-                            uint8_t distance = abs_distance(file, to_file) + abs_distance(rank, to_rank);
+					for (uint8_t to_file = min_file; to_file <= max_file; to_file++)
+					{
+						for (uint8_t to_rank = min_rank; to_rank <= max_rank; to_rank++)
+						{
+							Square to_square(to_file, to_rank);
 
-                            if (distance == 3)
-                            {
-                                rays[_r][index].set_by_square(to_square);
-                            }
-                        }
-                    }
-                    break;
-                }
+							uint8_t distance = abs_distance(file, to_file) + abs_distance(rank, to_rank);
 
-                case Ray::King:
-                {
-                    uint8_t min_file = (file == FILE_A) ? FILE_A : (file - 1);
-                    uint8_t max_file = (file == FILE_H) ? FILE_H : (file + 1);
+							if (distance == 3)
+							{
+								rays[_r][index].set_by_square(to_square);
+							}
+						}
+					}
+					break;
+				}
 
-                    uint8_t min_rank = (rank == RANK_1) ? RANK_1 : (rank - 1);
-                    uint8_t max_rank = (rank == RANK_8) ? RANK_8 : (rank + 1);
-                    
-                    for (uint8_t to_file = min_file; to_file <= max_file; to_file++)
-                    {
-                        for (uint8_t to_rank = min_rank; to_rank <= max_rank; to_rank++)
-                        {
-                            Square to_square(to_file, to_rank);
+				case Ray::King:
+				{
+					uint8_t min_file = (file == FILE_A) ? FILE_A : (file - 1);
+					uint8_t max_file = (file == FILE_H) ? FILE_H : (file + 1);
 
-                            if (from_square.get_data() != to_square.get_data())
-                            {
-                                rays[_r][index].set_by_square(to_square);
-                            }
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-    }
+					uint8_t min_rank = (rank == RANK_1) ? RANK_1 : (rank - 1);
+					uint8_t max_rank = (rank == RANK_8) ? RANK_8 : (rank + 1);
 
-    return rays;
+					for (uint8_t to_file = min_file; to_file <= max_file; to_file++)
+					{
+						for (uint8_t to_rank = min_rank; to_rank <= max_rank; to_rank++)
+						{
+							Square to_square(to_file, to_rank);
+
+							if (from_square.get_data() != to_square.get_data())
+							{
+								rays[_r][index].set_by_square(to_square);
+							}
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+
+	return rays;
 }();
 
-#endif // MOVEGEN_MOVEGEN_RAYS_HPP
+#endif  // MOVEGEN_MOVEGEN_RAYS_HPP
